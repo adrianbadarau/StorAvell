1c445a27375b19b4b5accdbd1de5fb08686ad88f
adrianbadarau <adi.badarau@yahoo.com>
2016-11-22 08:26:36 +0200

    added routes for all cms members

--- Modules/Cms/Http/routes.php
+++ Modules/Cms/Http/routes.php
@@ -10,4 +10,6 @@ Route::group(['middleware' => ['web'], 'prefix'=>'cms', 'namespace' => 'Modules\
 Route::group(['middleware' => ['web','role:admin,access_backend','menu'], 'prefix' => 'admin/cms', 'namespace' => 'Modules\Cms\Http\Controllers'], function()
 {
     Route::resource('page', 'PagesController',['except'=> ['show']]);
+    Route::resource('post', 'PostsController', ['except' => ['show']]);
+    Route::resource('category', 'CategoriesController', ['except' => ['show']]);
 });
